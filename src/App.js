5f57263456c5370eb4ed1dfd4f7c7a6c95e3fad1
viveknathani <viveknathani2402@gmail.com>
2021-12-02 17:20:03 +0530

    chore: setup form, clean public

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
 function App() {
+
+  const [user, setUser] = React.useState('');
+  const [result, setResult] = React.useState([]);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    let ele = document.getElementById('result');
+    ele.style.display = 'none';
+    const host = "https://vnxcollector.herokuapp.com";
+    fetch(`${host}/${user}`)
+    .then((res) => res.json())
+    .then((data) => {
+      setResult(JSON.parse(JSON.stringify(data)));
+      ele.style.display = 'block';
+    })
+    .catch((err) => console.log(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p id="tagline">collector 👨‍💻</p>
+      <p id="desc">
+        collector is a tool that shows a given user's top 5 GitHub contributions. Currently, only merged pull requests count as contributions. Ranking is based on how many forks the corresponding repository has.
+      </p>
+
+      <form>
+       <input id="user" type="text" className="searchTerm" placeholder="username" onChange={(e) => setUser(e.target.value)}/>
+       <button className="searchButton" onClick={(e) => onSubmit(e)}>
+         go!
+       </button>
+      </form>
+
+      <div id="result"> {
+        (() => {
+          if (result.length === 0) {
+            return(<p>we've got nothing here</p>)
+          } else {
+            return(
+              <table>
+                <tbody>
+                  <tr>
+                    <th>repo</th>
+                    <th>PR</th>
+                  </tr>
+                  {result.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.owner}/{item.repo}</td>
+                      <td><a href={item.url}>#{item.pr}</a></td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )  
+          }
+        })()
+      }
+      </div>
+
     </div>
   );
 }
